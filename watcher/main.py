802fa1186a1fc149887c1581c9bee65fe43c95b1
Kevin-Ratschinski <Kevin05774@hotmail.de>
2025-07-13 17:38:25 +0200

    Add logging configuration to main.py

--- watcher/main.py
+++ watcher/main.py
@@ -1,3 +1,14 @@
+import logging
+
+# setup basic logging configuration
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s -%(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+logger = logging.getLogger('watcher')
+
+
 def main():
     print("Hello from watcher!")
 
